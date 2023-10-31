fc3468c67bffd11848ce5565ac4f1bdad340f44c
Robin <mohammadrobin636@gmail.com>
2023-10-31 16:16:58 +0600

    first commit

---
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <section className='text-white bg-[#0f172a] h-screen flex justify-center items-center'>
+            <h1 className='text-center text-3xl'>Welcome To Gallery</h1>
+        </section>
+    );
+};
+
+export default Gallery;
