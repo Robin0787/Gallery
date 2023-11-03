d38ed10d6484304e987c980a6c502f3b0a7b22cc
Robin <mohammadrobin636@gmail.com>
2023-11-03 16:29:48 +0600

    Updated

--- src/Components/SingleImage/SingleImageCard.jsx
+++ src/Components/SingleImage/SingleImageCard.jsx
@@ -28,7 +28,9 @@ const SingleImageCard = ({ item }) => {
     return (
         <div id={item.id} className='relative w-full rounded-lg group duration-500 border-2 border-gray-300 dark:border-gray-600 cursor-grabbing gallery-item'>
             <img src={item?.img} className='rounded-md bg-white' />
-            <div className={`p-3 absolute ${item?.isSelected ? 'opacity-90' : 'opacity-0 group-hover:opacity-50'} top-0 left-0 w-full h-full rounded-md bg-black/40 duration-500`}>
+
+            <div className={`p-3 absolute ${item?.isSelected ? 'opacity-50' : 'opacity-0 group-hover:opacity-80'} top-0 left-0 w-full h-full rounded-md bg-black/40 duration-500`}>
+
                 <div className="flex justify-between items-center gap-4">
                     <input onChange={handleSelect} checked={item.isSelected} type="checkbox" className=" w-5 h-4 rounded-md cursor-pointer" />
                     <button onClick={handleDelete} className="text-white hover:text-red-500 rounded-lg duration-300">
--- src/Pages/Docs/Docs.jsx
+++ src/Pages/Docs/Docs.jsx
@@ -1,15 +1,117 @@
-import React from 'react';
+import { useRef, useState } from 'react';
+import { FaBars } from 'react-icons/fa';
 import Container from '../../Components/Container/Container';
 
+const dummyData = [
+    {
+        id: 1,
+        text: 'First'
+    },
+    {
+        id: 2,
+        text: 'Second'
+    },
+    {
+        id: 3,
+        text: 'Third'
+    },
+    {
+        id: 4,
+        text: 'Fourth'
+    },
+    {
+        id: 5,
+        text: 'Fifth'
+    }
+]
+
+
+
+
 const Docs = () => {
-    return (
-        <section className=''>
-            <Container className="text-black py-20 mt-10  rounded-md 
+    const [data, setData] = useState(dummyData);
+
+    const dragItem = useRef(null);
+    const dragOverItem = useRef(null);
+
+
+    const handleSort2 = () => {
+        // Duplicate the original array
+        let _data = [...data];
+
+        // remove and save the dragged item content
+        const draggedItemContent = _data.splice(dragItem.current, 1,)[0];
+
+        //switch the position 
+        _data.splice(dragOverItem.current, 0, draggedItemContent);
+
+        // reset the position of ref
+        dragItem.current = null;
+        dragOverItem.current = null;
+
+        console.log('Before sort');
+        // update the actual array
+        setData(_data);
+    }
+
+    const handleSort = () => {
+        console.log(dragItem.current, dragOverItem.current);
+        if (dragItem.current !== null && dragOverItem.current !== null) {
+            // Duplicate the original array
+            const _data = [...data];
+    
+            // Remove and save the dragged item content
+            const draggedItemContent = _data.splice(dragItem.current, 1)[0];
+    
+            // Insert the dragged item at the dragOverItem index
+            _data.splice(dragOverItem.current, 0, draggedItemContent);
+    
+            // Reset the position of refs
+            // dragItem.current = null;
+            dragOverItem.current = null;
+    
+            // Update the actual array
+            setData(_data);
+        }
+    }
+    
+const handleDragEnter = (e, index) => {
+    e.preventDefault();
+    dragOverItem.current = index;
+    handleSort();
+}
+
+return (
+    <section className=''>
+        <Container className="text-black py-20 mt-10  rounded-md 
             dark:text-white dark:bg-primary duration-300 shadow-[0_0_5px] shadow-gray-300 dark:shadow-gray-600">
-                <h1 className='text-center text-3xl'>Welcome To Documentation</h1>
-            </Container>
-        </section>
-    );
+            {/* <h1 className='text-center text-3xl'>Welcome To Documentation</h1> */}
+            <section className="flex justify-center items-center">
+                <div className='flex flex-col justify-center gap-4'>
+                    {
+                        data.map((item, index) => (
+                            <div key={item.id}
+                                // className='w-[200px] py-3 px-5 shadow-md border border-gray-200 rounded cursor-grab duration-500' 
+                                className={`w-[200px] py-3 px-5 shadow-md border border-gray-200 rounded cursor-grab transition-transform duration-300 transform ${dragOverItem.current === index ? 'scale-105' : ''}`}
+                                draggable
+                                onDragStart={(e) => dragItem.current = index}
+                                onDragEnter={(e) => handleDragEnter(e, index)}
+                                // onDragEnd={handleSort}
+                                onDrop={handleSort}
+                                onDragOver={(e) => e.preventDefault()}
+                            >
+                                <div className='flex justify-start items-center gap-6'>
+                                    <FaBars size={16} className='' />
+                                    <h1 className="text-lg">{item.text}</h1>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </section>
+        </Container>
+    </section>
+);
 };
 
 export default Docs;
